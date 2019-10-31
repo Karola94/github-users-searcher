8701343e0f16f910f88b9955ddd4f6d3ddab8136
Karolina Kuchta <linka_94@interia.eu>
2019-10-31 16:48:08 +0100

    Fix error

--- build.js
+++ build.js
@@ -1,6 +1,3 @@
-'use strict';
-
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
 "use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -155,3 +152,6 @@ var UsersList = function (_React$Component) {
 
   return UsersList;
 }(React.Component);
+'use strict';
+
+ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
